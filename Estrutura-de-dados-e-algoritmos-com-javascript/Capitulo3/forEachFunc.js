08938fe0f04bcd92a369965f90f223a5fd0537f8
RamonFossa <ramon.fossa@gmail.com>
2023-11-20 01:50:52 -0300

    feat: Incrementando na função forEach

--- Estrutura-de-dados-e-algoritmos-com-javascript/Capitulo3/forEachFunc.js
+++ Estrutura-de-dados-e-algoritmos-com-javascript/Capitulo3/forEachFunc.js
@@ -1,12 +1,11 @@
 function forEach(array, callback) {
     for (let i = 0; i < array.length; i++) {
-        callback(array[i]);
+        callback(array[i], i);
     }
 }
 
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reverse();
 
-forEach(arr, (num) => {
-    console.log('ALALALA');
-    console.log(num);
+forEach(arr, (num, index) => {
+    console.log(num, index);
 });
